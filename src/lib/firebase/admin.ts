cae23a273759902ae8e0e4ca1b7947d23e1edf64
vstudio935 <studio@visivo.in>
2026-08-22 16:17:44 +0000

    script added

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -9,10 +9,11 @@ export function getDb(): Firestore {
     const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
     // console.log("Original:");
     // console.log(process.env.FIREBASE_PRIVATE_KEY);
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
-    // const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\\\n/g, "\n").replace(
-    //   /\\n/g,
-    //   "\n",);
+    // const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n");
+    const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\\\n/g, "\n").replace(
+      /\\n/g,
+      "\n",
+    );
     // console.log("After process:");
     // console.log(privateKey);
 
